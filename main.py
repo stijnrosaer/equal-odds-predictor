7452c403fa081c40272404c13811751b5e2253dc
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2021-03-08 14:11:29 +0100

    add output images

--- main.py
+++ main.py
@@ -1,109 +1,92 @@
-import random
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import make_classification
+from scipy.special import expit
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 10, 1).reshape(-1, 1)
+y = np.array([0, 0, 0, 1, 1, 0, 1, 1, 1, 1])
+
+model = LogisticRegression(solver='liblinear')
+model.fit(x, y)
+
+x_test = np.arange(0, 10, 0.01)
+y_test = x_test * model.coef_ + model.intercept_
+
+sigmoid = expit(y_test)
+
+y_test_val = np.array([0, 0, 0, 1, 1, 0, 1, 1, 1])
+x_test_val = [0, 1, 1.5, 3, 4, 5, 7, 8, 9]
+color = ["blue", "blue", "blue", "green", "green", "red", "green", "green", "green"]
 
 
-class Data:
-    def __init__(self):
-        self.A = random.randint(0, 1)
-        self.R = random.random()
+plt.scatter(x_test_val, y_test_val, c=color)
 
-        self.Y = 0
-        r = random.random()
-        if self.A == 1:
-            if self.R > 0.6:
-                self.Y = random.randint(0,1)
-        else:
-            if self.R > 0.8:
-                self.Y = random.randint(0,1)
+plt.plot(x_test, sigmoid.ravel(), label="logistic fit")
+plt.hlines(0.5, 0, 10, linestyles={'dashed'}, color="gray", label="treshold= 0.5 ")
+plt.vlines(2.1, 0, 1, linestyles={'dotted'}, color="gray")
+plt.scatter(5,0, c="red", label="misclassified")
 
-    def __str__(self):
-        return f"A: {self.A}, Y: {self.Y} R: {self.R}"
-
-    def __repr__(self):
-        return str(self)
-
-
-def plot(data, sensitive_value):
-    data_val = [i for i in data if i.A == sensitive_value]
-
-    """
-    fpr_tpr = []
-    total_pos = len([i for i in data_val if i.Y == 1])
-    total_neg = len([i for i in data_val if i.Y == 0])
-
-    for threshold in range(len(data_val)):
-        tp = fp = tn = fn = 0
-        for item in data_val[:threshold]:  # ^y = 0
-            if item.Y == 1:
-                fp += 1
-            else:
-                tn += 1
-        for item in data_val[threshold:]:  # ^y = 1
-            if item.Y == 1:
-                tp += 1
-            else:
-                fn += 1
-
-        try:
-            tpr = tp/(tp + fn)
-        except:
-            tpr = 0
-        try:
-            fpr = fp/(tn + fp)
-        except:
-            fpr = 0
-        fpr_tpr.append((fpr, tpr))
-    """
-
-
-    a,b,c = roc_curve([i.Y for i in data_val], [i.R for i in data_val])
-    plt.plot(a, b)
-    plt.plot([0,1], [0,1])
-    plt.xlabel("False positive ratio")
-    plt.ylabel("True positive ratio")
-    # plt.xlim([0,1])
-    # plt.ylim([0, 1])
-    plt.show()
+plt.ylabel("probability of obesity")
+plt.xlabel("weight of animal")
+plt.legend(loc="lower right")
+plt.show()
 
 
 
-if __name__ == '__main__':
+# a-conditional
+y_m = np.array([0, 0, 0, 0, 1, 1, 0, 1, 1])
+x_m = np.array([0, 1, 1.5, 3, 4, 5, 6, 7, 9])*1.2
+color_m = ["blue" for _ in range(len(y_test_val))]
 
-    X, y = make_classification(n_samples=1000, n_features=2, n_redundant=0, weights=[0.5])
-
-    trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2, stratify=y)
-
-    model = LogisticRegression(solver="lbfgs")
-    model.fit(trainX, trainy)
-    yhat = model.predict_proba(testX)
-
-    # keep probabilities for the positive outcome only
-    yhat = yhat[:, 1]
-    # calculate roc curves
-    fpr, tpr, thresholds = roc_curve(testy, yhat)
-    # calculate the g-mean for each threshold
-    gmeans = np.sqrt(tpr * (1 - fpr))
-    # locate the index of the largest g-mean
-    # ix = argmax(gmeans)
-    # print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
-    # plot the roc curve for the model
-    plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill')
-    plt.plot(fpr, tpr, marker='.', label='Logistic')
-    # plt.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best')
-    # axis labels
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.legend()
-    # show the plot
-    plt.show()
+y_f = np.array([0, 0, 0, 1, 1, 0, 1, 1, 1])
+x_f = [0, 1, 1.5, 3, 4, 5, 7, 8, 9]
+color_f = ["red" for _ in range(len(y_test_val))]
 
 
-    # data = [Data() for _ in range(1000)]
-    #
-    # plot(data, 0)
-    exit(0)
+plt.scatter(x_m, y_m, c=color_m, label="male weight")
+plt.scatter(x_f, y_f, c=color_f, label="female weight")
+
+plt.plot(x_test, sigmoid.ravel(), label="logistic fit")
+plt.hlines(0.5, 0, 10, linestyles={'dashed'}, color="gray", label="treshold= 0.5 ")
+plt.vlines(2.1, 0, 1, linestyles={'dotted'}, color="gray")
+
+plt.ylabel("probability of obesity")
+plt.xlabel("weight of animal")
+plt.legend(loc="lower right")
+plt.show()
+
+#a-cond ROC
+fpr_m, tpr_m, thresholds_m = roc_curve(y_m, list(x_m), drop_intermediate=False)
+
+plt.plot(fpr_m, tpr_m, label="male", c="blue")
+# for i in range(len(fpr_m)):
+#     plt.annotate(round(thresholds_m[i], 2), (fpr_m[i]+0.01, tpr_m[i]+0.01))
+
+fpr_f, tpr_f, thresholds_f = roc_curve(y_f, x_f, drop_intermediate=False)
+
+plt.plot(fpr_f, tpr_f, label="female", c="red")
+# for i in range(len(fpr_f)):
+#     plt.annotate(round(thresholds_f[i], 2), (fpr_f[i]+0.01, tpr_f[i]+0.01))
+
+plt.xlabel("False positive rate (FPR)")
+plt.ylabel("True positive rate (TPR)")
+plt.title("A-conditional ROC curve")
+plt.legend(loc="lower right")
+plt.show()
+
+#ROC
+join_y = [*y_m , *y_f]
+join_x = [*x_m , *x_f]
+fpr, tpr, thresholds = roc_curve(join_y, join_x, drop_intermediate=False)
+
+plt.plot(fpr, tpr)
+for i in range(len(fpr)):
+    plt.annotate(round(thresholds[i],2), (fpr[i]+0.01, tpr[i]+0.01))
+plt.fill_between(fpr_1.sample(1000), (tpr_1.sample(1000)), fpr_2.sample(1000),tpr_2.sample(1000), alpha=0.3)
+plt.legend()
+plt.xlabel("False positive rate (FPR)")
+plt.ylabel("True positive rate (TPR)")
+plt.title("ROC curve")
+plt.show()
